80135cc5a318a74cd76fa4dfc2de9b2874fc0571
ry-zhao <ryanzhao21@gmail.com>
2021-09-16 17:24:12 -0700

    New bird game.

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
-var $car = document.querySelector('.car');
-var offsetLeft = 0;
-var offsetTop = 0;
+var $car1 = document.querySelector('#car1');
+var $car2 = document.querySelector('.car2');
+
+var modelCar1 = {
+  offsetLeft: 0,
+  offsetTop: 0
+};
+
 var driveInterval = null;
 
-document.addEventListener('keydown', driveCar);
+document.addEventListener('keydown', driveOrStopCar);
 
-function driveCar(event) {
+function driveOrStopCar(event) {
   if (event.code !== 'Space') {
     return;
   }
-  driveInterval = setInterval(moveCar, 16);
+
+  if (driveInterval === null) {
+    driveInterval = setInterval(moveCar, 16);
+  } else {
+    clearInterval(driveInterval);
+    driveInterval = null;
+  }
 }
 
 function moveCar(event) {
-  switch ($car.className) {
+  switch ($car1.className) {
     case 'car right':
-      offsetLeft += 5;
-      $car.style.left = offsetLeft + 'px';
+      modelCar1.offsetLeft += 8;
+      $car1.style.left = modelCar1.offsetLeft + 'px';
       break;
     case 'car down':
-      offsetTop += 5;
-      $car.style.top = offsetTop + 'px';
+      modelCar1.offsetTop += 8;
+      $car1.style.top = modelCar1.offsetTop + 'px';
       break;
     case 'car left':
-      offsetLeft -= 5;
-      $car.style.left = offsetLeft + 'px';
+      modelCar1.offsetLeft -= 8;
+      $car1.style.left = modelCar1.offsetLeft + 'px';
       break;
     case 'car up':
-      offsetTop -= 5;
-      $car.style.top = offsetTop + 'px';
+      modelCar1.offsetTop -= 8;
+      $car1.style.top = modelCar1.offsetTop + 'px';
   }
 }
 
@@ -37,18 +48,61 @@ document.addEventListener('keydown', turnCar);
 function turnCar(event) {
   switch (event.code) {
     case 'ArrowRight':
-      $car.className = 'car right';
+      $car1.className = 'car right';
+      $car1.style.transform = 'rotate(45deg)';
       break;
     case 'ArrowDown':
-      $car.className = 'car down';
+      $car1.className = 'car down';
       break;
     case 'ArrowLeft':
-      $car.className = 'car left';
+      $car1.className = 'car left';
+      $car1.style.transform = 'scaleX(-1) rotate(45deg)';
       break;
     case 'ArrowUp':
-      $car.className = 'car up';
+      $car1.className = 'car up';
       break;
     default:
       break;
   }
 }
+
+// function moveCar(event) {
+//   switch ($car1.className) {
+//     case 'car right':
+//       modelCar1.offsetLeft += 8;
+//       $car1.style.left = modelCar1.offsetLeft + 'px';
+//       break;
+//     case 'car down':
+//       modelCar1.offsetTop += 8;
+//       $car1.style.top = modelCar1.offsetTop + 'px';
+//       break;
+//     case 'car left':
+//       modelCar1.offsetLeft -= 8;
+//       $car1.style.left = modelCar1.offsetLeft + 'px';
+//       break;
+//     case 'car up':
+//       modelCar1.offsetTop -= 8;
+//       $car1.style.top = modelCar1.offsetTop + 'px';
+//   }
+// }
+
+// document.addEventListener('keydown', turnCar);
+
+// function turnCar(event) {
+//   switch (event.code) {
+//     case 'ArrowRight':
+//       $car1.className = 'car right';
+//       break;
+//     case 'ArrowDown':
+//       $car1.className = 'car down';
+//       break;
+//     case 'ArrowLeft':
+//       $car1.className = 'car left';
+//       break;
+//     case 'ArrowUp':
+//       $car1.className = 'car up';
+//       break;
+//     default:
+//       break;
+//   }
+// }
